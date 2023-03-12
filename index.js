d2ac1199f4f4cca2a20e78bec45cd4204f7b4577
demitryedwards <dxyz773@gmail.com>
2023-03-12 16:56:14 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+  let card = [];
+  for (let i = 0; i < name.length; i++) {
+    card.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+  return card;
+}
+
+function countDown(integer) {
+  while (integer >= 0) {
+    console.log(integer);
+    integer--;
+  }
+}
+
+countDown(10);
